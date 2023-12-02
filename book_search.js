f557e72fd4c35b7541b7e07bbbe76a6e09fdf84e
KEG <110787383+kgoggio@users.noreply.github.com>
2023-12-02 14:59:27 -0600

    Create book_search.js

---
+++ book_search.js
@@ -0,0 +1,86 @@
+function findSearchTermInBooks(searchTerm, scannedTextObj) {
+    var result = {
+        "SearchTerm": searchTerm,
+        "Results": []
+    };
+
+    // Iterate over each book
+    scannedTextObj.forEach(book => {
+        // Flag to check if the term is found
+        let found = false;
+
+        // Iterate over each content block
+        book.Content.forEach(content => {
+            // Check if the search term is in the text and not already found
+            if (!found && content.Text.includes(searchTerm)) {
+                // Set found to true after the first occurrence
+                found = true;
+
+                // Add result to array
+                result.Results.push({
+                    "ISBN": book.ISBN,
+                    "Page": content.Page,
+                    "Line": content.Line
+                });
+            }
+        });
+    });
+
+    return result;
+}
+
+
+const twentyLeaguesIn = [
+    {
+        "Title": "Twenty Thousand Leagues Under the Sea",
+        "ISBN": "9780000528531",
+        "Content": [
+            {
+                "Page": 31,
+                "Line": 8,
+                "Text": "now simply went on by her own momentum.  The dark-"
+            },
+            {
+                "Page": 31,
+                "Line": 9,
+                "Text": "ness was then profound; and however good the Canadian\'s"
+            },
+            {
+                "Page": 31,
+                "Line": 10,
+                "Text": "eyes were, I asked myself how he had managed to see, and"
+            } 
+        ] 
+    }
+]
+    
+/** Example output object */
+const twentyLeaguesOut = {
+    "SearchTerm": "the",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        }
+    ]
+}
+
+const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
+    console.log("PASS: Test 1");
+} else {
+    console.log("FAIL: Test 1");
+    console.log("Expected:", twentyLeaguesOut);
+    console.log("Received:", test1result);
+}
+
+/** We could choose to check that we get the right number of results. */
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
+if (test2result.Results.length == 1) {
+    console.log("PASS: Test 2");
+} else {
+    console.log("FAIL: Test 2");
+    console.log("Expected:", twentyLeaguesOut.Results.length);
+    console.log("Received:", test2result.Results.length);
+}
